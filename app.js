d7d8c5a978137eabfd0b391fa0b95d5bfac31de6
BhaskarT18 <bhaskartikale@gmail.com>
2024-08-05 14:27:54 +0530

    add modules

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date= require(__dirname+ "/date.js")
+
 const app = express();
-let items=["buy food","make food","eat food"];
+const items=["buy food","make food","eat food"];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    let today = new Date();
-    let option ={
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-
-    }
-    let day= today.toLocaleDateString("en-US", option);
+    const day= date.getDate();
     res.render("list", { kindOfDay: day, newListItems: items });
 });
 
 app.post("/", function(req,res){
-    let item=req.body.addItem;
+    const item=req.body.addItem;
     items.push(item)
     res.redirect("/");
 });
