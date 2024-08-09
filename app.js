a1caa1c854e4cd7764d840812de6d6988285601d
BhaskarT18 <bhaskartikale@gmail.com>
2024-08-09 20:01:56 +0530

    we have create mongodb and implemented

--- app.js
+++ app.js
@@ -1,22 +1,133 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date= require(__dirname+ "/date.js")
-
+const mongoose = require("mongoose");
 const app = express();
-const items=["buy food","make food","eat food"];
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
-    const day= date.getDate();
-    res.render("list", { kindOfDay: day, newListItems: items });
+//db connection
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+//schema
+const itemSchema = {
+    name: String
+};
+
+//model
+const Item = mongoose.model("Item", itemSchema);
+
+//list of data
+const item1 = new Item({
+    name: "Welcome to your ToDo List."
 });
 
-app.post("/", function(req,res){
-    const item=req.body.addItem;
-    items.push(item)
-    res.redirect("/");
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+app.get('/', function (req, res) {
+    Item.find({})
+        .then((foundItems) => {
+            if (foundItems.length === 0) {
+                Item.insertMany(defaultItems)
+                    .then(() => console.log('Default items added successfully'))
+                    .catch(err => console.error('Error inserting default items', err));
+                res.redirect("/");
+            } else {
+                res.render("list", { kindOfDay: "Today", newListItems: foundItems });
+            }
+        })
+        .catch(err => {
+            console.error(err);
+        });
+});
+
+app.get("/:customListName", (req, res) => {
+    const customListName = req.params.customListName;
+    
+    List.findOne({ name: customListName })
+        .then((foundList) => {
+            if (foundList) { // if list exists, render it
+                res.render("list", { kindOfDay: foundList.name, newListItems: foundList.items });
+            } else { // if not, create a new list
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+                list.save();
+                res.redirect("/" + customListName);
+            }
+        })
+        .catch((err) => {
+            console.error("Error:", err);
+        });
+});
+
+app.post("/", function (req, res) {
+    const itemName = req.body.addItem;
+    const listName = req.body.addList;
+
+    const item = new Item({
+        name: itemName
+    });
+
+    if (listName === "Today") {
+        item.save();
+        res.redirect("/");
+    } else {
+        List.findOne({ name: listName })
+            .then((foundList) => {
+                foundList.items.push(item);
+                foundList.save();
+                res.redirect("/" + listName);
+            })
+            .catch(err => {
+                console.error("Error:", err);
+            });
+    }
+});
+
+app.post("/delete", function (req, res) {
+    const checkItemId = req.body.checkbox;
+    const listName = req.body.listName;  
+
+    if (listName === "Today") {
+        Item.findByIdAndDelete(checkItemId)
+            .then(() => {
+                console.log("Successfully deleted checked item");
+                res.redirect("/");  
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    } else {
+        List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkItemId } } })
+            .then((foundList) => {
+                if (foundList) {
+                    res.redirect("/" + listName);
+                } else {
+                    console.error("List or item not found");
+                }
+            })
+            .catch((err) => {
+                console.error("Error removing item:", err);
+            });
+    }
 });
 
 app.listen(5050, function () {
